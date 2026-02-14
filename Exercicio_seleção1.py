d0f2d962ef5568ae3624dd7a3f22b1a2c8f7ad29
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-02-14 10:42:07 -0300

    Update Exercicio_seleção1.py

---
+++ Exercicio_seleção1.py
@@ -0,0 +1,10 @@
+dia = int(input("Digite um número de 1 a 3: "))
+
+if dia == 1:
+    print("Segunda-feira")
+elif dia == 2:
+    print("Terça-feira")
+elif dia == 3:
+    print("Quarta-feira")
+else:
+    print("Número inválido")
