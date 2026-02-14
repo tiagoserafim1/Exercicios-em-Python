5b90b64e200b88f1daee1eac1bb52e445f126fd0
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-02-14 10:42:56 -0300

    Add temperature evaluation script

---
+++ Exercicio_seleção11.py
@@ -0,0 +1,8 @@
+temperatura = float(input("Digite a temperatura atual: "))
+
+if temperatura < 15:
+    print("Está frio")
+elif temperatura <= 25:
+    print("Clima agradável")
+else:
+    print("Está quente")
