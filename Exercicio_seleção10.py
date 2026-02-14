3e60907c98ebb44383ec51096f1b68b3397520c1
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-02-14 10:42:26 -0300

    Update Exercicio_seleção10.py

---
+++ Exercicio_seleção10.py
@@ -0,0 +1,10 @@
+personagem = input("Escolha um personagem (guerreiro, mago, arqueiro): ")
+
+if personagem == "guerreiro":
+    print("Você escolheu o Guerreiro! Força alta 💪")
+elif personagem == "mago":
+    print("Você escolheu o Mago! Magia poderosa 🔮")
+elif personagem == "arqueiro":
+    print("Você escolheu o Arqueiro! Precisão elevada 🎯")
+else:
+    print("Personagem inválido.")
